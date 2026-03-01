9e45a1880993e947820e9f3b470d171b223decd7
Mohsin <oxgmohsin@gmail.com>
2026-03-01 11:09:48 +0600

    Watch Details page added

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./MainLayout.module.css";
 import TopNav from "../components/top-navbar/TopNav";
 import Home from "../pages/home/Home";
+import WatchDetails from "../pages/watch-details/WatchDetails";
 
 export default function MainLayout() {
   return (
@@ -9,6 +10,7 @@ export default function MainLayout() {
       <TopNav />
       <div className={styles.main}>
         <Home />
+        <WatchDetails />
       </div>
     </div>
   );
